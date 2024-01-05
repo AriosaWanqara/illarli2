498be02e02c34584634071d1a01dc4751fe8518a
AriosaWanqara <jariosa@wanqara.com>
2024-01-05 17:01:50 -0500

    add redirect in root route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import { authRoutes } from "@/modules/auth/route";
-import { posRoutes } from "@/modules/pos/route";
 import { dashboardRoutes } from "@/modules/dashboard/route";
+import { posRoutes } from "@/modules/pos/route";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +9,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      redirect: { name: "dashboard-home" },
     },
     ...authRoutes,
     ...posRoutes,
