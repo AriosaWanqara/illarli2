c36dc6674bcd1a5b7e1638293f7b821a7702e265
AriosaWanqara <jariosa@wanqara.com>
2024-01-11 12:36:48 -0500

    init image editor

--- src/modules/dashboard/components/shared/FileUpload.vue
+++ src/modules/dashboard/components/shared/FileUpload.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
-import vueFilePond from "vue-filepond";
-import "filepond/dist/filepond.min.css";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
-import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginImageEditor from "@pqina/filepond-plugin-image-editor";
 import FilePondPluginFilePoster from "filepond-plugin-file-poster";
-import FilePondPluginImageResize from "filepond-plugin-image-resize";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import "filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+import "filepond/dist/filepond.min.css";
+import vueFilePond from "vue-filepond";
+// Import Pintura styles
+import "@pqina/pintura/pintura.css";
+
+import {
+  createDefaultImageOrienter,
+  createDefaultImageReader,
+  createDefaultImageWriter,
+  legacyDataToImageState,
+  locale_en_gb,
+  markup_editor_defaults,
+  markup_editor_locale_en_gb,
+
+  // filepond
+  openEditor,
+  plugin_annotate,
+  plugin_annotate_locale_en_gb,
+  plugin_crop,
+  plugin_crop_locale_en_gb,
+  plugin_filter,
+  plugin_filter_defaults,
+  plugin_filter_locale_en_gb,
+  plugin_finetune,
+  plugin_finetune_defaults,
+  plugin_finetune_locale_en_gb,
+  processImage,
+  // plugins
+  setPlugins,
+} from "@pqina/pintura";
+
+setPlugins(plugin_crop, plugin_finetune, plugin_filter, plugin_annotate);
 
 interface props {
   files: any[];
@@ -18,10 +49,10 @@ const props = defineProps<props>();
 const emits = defineEmits(["load-base64"]);
 
 const FilePond = vueFilePond(
-  FilePondPluginFileValidateType,
-  FilePondPluginImagePreview,
+  FilePondPluginImageEditor,
   FilePondPluginFilePoster,
-  FilePondPluginImageResize
+  FilePondPluginFileValidateType,
+  FilePondPluginImagePreview
 );
 
 const server = {
@@ -43,6 +74,40 @@ const server = {
       .catch((err) => {
         console.log(err);
       });
+  },
+};
+
+const editor = {
+  // map legacy data objects to new imageState objects
+  legacyDataToImageState: legacyDataToImageState,
+
+  // used to create the editor, receives editor configuration, should return an editor instance
+  createEditor: openEditor,
+
+  // Required, used for reading the image data
+  imageReader: [createDefaultImageReader],
+
+  // optionally. can leave out when not generating a preview thumbnail and/or output image
+  imageWriter: [createDefaultImageWriter],
+
+  // used to generate poster images, runs an editor in the background
+  imageProcessor: processImage,
+
+  // editor options
+  editorOptions: {
+    utils: ["crop", "finetune", "filter", "annotate"],
+    imageOrienter: createDefaultImageOrienter(),
+    ...plugin_finetune_defaults,
+    ...plugin_filter_defaults,
+    ...markup_editor_defaults,
+    locale: {
+      ...locale_en_gb,
+      ...plugin_crop_locale_en_gb,
+      ...plugin_finetune_locale_en_gb,
+      ...plugin_filter_locale_en_gb,
+      ...plugin_annotate_locale_en_gb,
+      ...markup_editor_locale_en_gb,
+    },
   },
 };
 
@@ -68,6 +133,7 @@ const handleLoad = async (event: any) => {
 <template>
   <file-pond
     ref="pond"
+    :filePosterHeight="imagePreviewHeight"
     :imagePreviewHeight="imagePreviewHeight"
     :credits="false"
     class="custome"
