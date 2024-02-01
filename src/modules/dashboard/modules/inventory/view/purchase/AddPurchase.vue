6168827153d29eabb5aad1a38e76a03801b6e312
AriosaWanqara <jariosa@wanqara.com>
2024-02-01 16:02:29 -0500

    create purchase banner

--- src/modules/dashboard/modules/inventory/view/purchase/AddPurchase.vue
+++ src/modules/dashboard/modules/inventory/view/purchase/AddPurchase.vue
@@ -7,22 +7,41 @@ import { personRolEnum } from "../../../persons/constant/personRolEnum";
 import CreatePurchaseMinGeneralInfo from "../../components/purchase/create/CreatePurchaseMinGeneralInfo.vue";
 import ProductSelector from "../../components/purchase/create/ProductSelector.vue";
 import type { PurchaseToSave2 } from "../../models/PurchaseOrder";
+import UIParentCardV2 from "@/modules/dashboard/components/shared/UIParentCardV2.vue";
+import purchaseBanner from "@dashboard/assets/images/purchase_banner.png";
 
 const person = ref<Provider>({} as Provider);
 const purchase = ref<PurchaseToSave2>({} as PurchaseToSave2);
 </script>
 
 <template>
-  <UIScaffold title="Compras">
+  <!-- <UIScaffold title="Compras">
     <template #default>
-      <!-- <CreatePersonWithMinInfoForm
+      <CreatePersonWithMinInfoForm
         :person="person"
         :person-type="personRolEnum.SUPPLIER"
-      /> -->
-      <!-- <CreatePurchaseMinGeneralInfo :purchase="purchase" /> -->
+      />
       <ProductSelector />
     </template>
-  </UIScaffold>
+  </UIScaffold> -->
+  <div class="tw-px-4">
+    <UIParentCardV2>
+      <template #title>
+        <div class="tw-h-[100px]">
+          <div class="tw-h-[70px] tw-flex tw-justify-center tw-bg-[#7BD599]">
+            <div
+              class="tw-bg-white tw-rounded-full tw-border-[1px] tw-border-[#7BD599] tw-grid tw-place-content-center tw-h-[90px] tw-w-[90px]"
+            >
+              <VImg :src="purchaseBanner" width="120" />
+            </div>
+          </div>
+        </div>
+      </template>
+      <div class="tw-p-4">
+        <CreatePurchaseMinGeneralInfo :purchase="purchase" />
+      </div>
+    </UIParentCardV2>
+  </div>
 </template>
 
 <style scoped></style>
